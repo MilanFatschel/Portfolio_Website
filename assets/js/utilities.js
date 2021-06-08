e97f9cc1ad4a578cf46f8365bc6e796e47f75e97
Milan Fatschel <31837228+MilanFatschel@users.noreply.github.com>
2021-06-08 16:55:50 -0700

    Add files via upload

---
+++ assets/js/utilities.js
@@ -0,0 +1,12 @@
+  // Href click listeners
+  function customHREF(url) {
+    window.open(url, '_blank');
+  }
+
+
+  function scrollToTop() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  }
